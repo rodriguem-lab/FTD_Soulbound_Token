d755195d9d7d03369d0288888c303aab313ea79e
rodriguem-lab <rodriguemzt@gmail.com>
2026-02-20 20:57:50 +0100

    Add revoke function to allow token revocation

--- FTD_Soulbound.sol
+++ FTD_Soulbound.sol
@@ -19,6 +19,97 @@ contract FTDSoulbound is ERC721, Ownable {
 
     event Minted(address indexed student, uint256 indexed tokenId);
     event CohortURIUpdated(string newURI);
+    event Revoked(uint256 indexed tokenId, address indexed student);
+
+    constructor(string memory _programName, uint256 _cohortYear)
+        ERC721("FTD Soulbound Token", "FTDSBT")
+        Ownable(msg.sender)
+    {
+        programName = _programName;
+        cohortYear = _cohortYear;
+        _cohortURI = "ipfs://bafkreicpstfrprvxpoumwqunxjquyyzt6ujz66dtf5dzlgvg4xynu2yidy";
+    }
+
+    function mint(address student) external onlyOwner {
+        require(student != address(0), "Invalid student");
+        require(!hasMinted[student], "Already minted");
+
+        _mintProcess(student);
+    }
+
+    // Mint en batch (cohorte) optimisé
+    function mintBatch(address[] calldata students) external onlyOwner {
+        uint256 len = students.length;
+        require(len > 0, "Empty list");
+
+        for (uint256 i = 0; i < len; i++) {
+            address student = students[i];
+            if (student != address(0) && !hasMinted[student]) {
+                _mintProcess(student);
+            }
+        }
+    }
+
+    // Fonction interne pour factoriser le mint
+    function _mintProcess(address student) internal {
+        uint256 tokenId = nextId++;
+        hasMinted[student] = true;
+        _safeMint(student, tokenId);
+        emit Minted(student, tokenId);
+    }
+
+    // RÉVOCATION CORRIGÉE : Libère le statut hasMinted
+    function revoke(uint256 tokenId) external onlyOwner {
+        address student = ownerOf(tokenId); // On récupère l'étudiant avant de brûler le token
+        
+        hasMinted[student] = false; // L'étudiant peut à nouveau recevoir un token
+        _burn(tokenId);
+        
+        emit Revoked(tokenId, student);
+    }
+
+    function setCohortURI(string calldata newURI) external onlyOwner {
+        _cohortURI = newURI;
+        emit CohortURIUpdated(newURI);
+    }
+
+    function tokenURI(uint256) public view override returns (string memory) {
+        return _cohortURI;
+    }
+
+    // Logique Soulbound (non-transférable)
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override
+        returns (address)
+    {
+        address from = _ownerOf(tokenId);
+        if (from != address(0) && to != address(0)) {
+            revert("Soulbound: transfers disabled");
+        }
+        return super._update(to, tokenId, auth);
+    }
+}// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/access/Ownable.sol";
+
+contract FTDSoulbound is ERC721, Ownable {
+    uint256 public nextId = 1;
+
+    // 1 SBT max par étudiant
+    mapping(address => bool) public hasMinted;
+
+    // Infos cohorte
+    string public programName;
+    uint256 public cohortYear;
+
+    // URI du metadata IPFS (cohorte)
+    string private _cohortURI;
+
+    event Minted(address indexed student, uint256 indexed tokenId);
+    event CohortURIUpdated(string newURI);
     event Revoked(uint256 indexed tokenId);
 
     constructor(string memory _programName, uint256 _cohortYear)
